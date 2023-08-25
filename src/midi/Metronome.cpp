b61e060944fc68a661c1b3c7c2f12b9368715c94
Meowchestra <42126400+Meowchestra@users.noreply.github.com>
2023-08-25 11:18:56 -0700

    Update Metronome.cpp

--- src/midi/Metronome.cpp
+++ src/midi/Metronome.cpp
@@ -23,7 +23,7 @@ Metronome::Metronome(QObject *parent) :	QObject(parent) {
     _player = new QSoundEffect(this);
 #endif
     _player->setVolume(1.0);
-    _player->setSource(QUrl("qrc:/run_environment/metronome/metronome-01.wav"));
+    _player->setSource(QUrl("qrc:/metronome/metronome-01.wav"));
 }
 
 void Metronome::setFile(MidiFile *file){
